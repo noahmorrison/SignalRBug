0a477d4af902c608fc137682b28970a0c9e81759
Noah Morrison <nmorrison@arsproducts.us>
2019-07-16 12:01:46 -0400

    Fix the == operator on PingClass

--- SignalRBug/PingClass.cs
+++ SignalRBug/PingClass.cs
@@ -9,5 +9,16 @@ namespace SignalRBug
         {
             return $"{Type} ({Message})";
         }
+
+        public static bool operator ==(PingClass self, PingClass other)
+        {
+            return self.Type == other.Type
+                && self.Message == other.Message;
+        }
+
+        public static bool operator !=(PingClass self, PingClass other)
+        {
+            return !(self == other);
+        }
     }
 }
